ce74289f57454afbef191b7e03948845bdb21717
MsrdMai <sirilada.mai@gmail.com>
2021-11-16 20:59:43 +0700

    store repair v1.1

--- src/contents/Store.js
+++ src/contents/Store.js
@@ -53,7 +53,7 @@ class Store extends Component {
                       display: "block",
                       borderRadius: "0%",
                       boxShadow: "0 4px 8px 0 rgba(0, 0, 0, 0.2), 0 6px 20px 0 rgba(0, 0, 0, 0.19)",
-                      maxWidth: "370px",
+                      maxWidth: "320px",
                       maxHeight: "300px",
                     }}
                   ></img>
@@ -196,7 +196,7 @@ class Store extends Component {
                 <div className="google-map-code border-5 border-green-200">
                   <iframe
                     src="https://www.google.com/maps/embed?pb=!1m18!1m12!1m3!1d15282225.79979123!2d73.7250245393691!3d20.750301298393563!2m3!1f0!2f0!3f0!3m2!1i1024!2i768!4f13.1!3m3!1m2!1s0x30635ff06b92b791%3A0xd78c4fa1854213a6!2sIndia!5e0!3m2!1sen!2sin!4v1587818542745!5m2!1sen!2sin"
-                    width="370"
+                    width="320"
                     height="400"
                     frameborder="0"
                     style={{ border: 0 }}
@@ -208,7 +208,7 @@ class Store extends Component {
 
               <div class="col">
               <br></br><br></br><br></br>
-              <p class="pt-3 text-2xl font-black bg-yellow-100"
+              <p class="pt-3 text-2xl font-black"
                style={{
               alignItems: "center",
               textAlign: "center",
